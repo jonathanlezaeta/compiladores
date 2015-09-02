a292ff4537f2be2ac1d4f17fabe34236379cc136
Jonathan <jonathan.lezaeta@gmail.com>
2015-09-02 19:58:13 -0300

    Interfaz grafica

--- CompiladorV1/src/main.java
+++ CompiladorV1/src/main.java
@@ -1,12 +1,16 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
-public class main {
+public class main {    // Your class name
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+   public static void main(String[] args) {
+      
+      JFrame f = new JFrame("A JFrame");
+      f.setSize(250, 250);
+      f.setLocation(300,200);
+      f.getContentPane().add(BorderLayout.CENTER, new JTextArea(10, 40));
+      f.setVisible(true);
+      
+    }
 }
